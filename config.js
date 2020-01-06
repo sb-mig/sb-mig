afe6968cfe52ae316bfa33e82424a4950f835419
Marcin Krawczyk <marcin.krawczyk@ef.com>
2020-01-06 15:40:34 +0100

    [#5] add experimental feature to download code from seed project (schema based components and match-storyblok-react components

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 
 const defaultConfig = {
-  presetsDirectory: "presets",
   oauthToken: process.env.STORYBLOK_OAUTH_TOKEN,
   spaceId: process.env.STORYBLOK_SPACE_ID,
-  accessToken: process.env.STORYBLOK_ACCESS_TOKEN
+  accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
+  githubToken: process.env.GITHUB_TOKEN,
+  seedRepo: process.env.SEED_REPO
 };
 
 const storyblokApiUrl = 'https://api.storyblok.com/v1';
