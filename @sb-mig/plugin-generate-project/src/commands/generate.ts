4b868d850ed150f6763ecc6471917b7aa6261372
Marcin Krawczyk <marckraw@icloud.com>
2020-04-30 17:12:05 +0200

    feat: generate command now works, it is creating storyblok space and installing components using add-components methods

--- @sb-mig/plugin-generate-project/src/commands/generate.ts
+++ @sb-mig/plugin-generate-project/src/commands/generate.ts
@@ -1,36 +1,37 @@
-// @ts-ignore
-import * as yarnOrNpm from 'yarn-or-npm';
-import * as execa from 'execa';
-import * as ora from 'ora';
+import ora from 'ora';
 import { flags } from '@oclif/command'
 import Command from 'sb-mig/lib/core'
 import { installComponentCommand, installProvidedComponents } from '@sb-mig/plugin-add-components/lib/api/add';
+import { copyComponents } from '@sb-mig/plugin-add-components/lib/api/copy';
+import { updateComponentsJs } from '@sb-mig/plugin-add-components/lib/api/update';
 import { cloneRepo, createSpace, removeAndModifyFiles } from '../api';
 
 export default class Hello extends Command {
   static description = 'Generate whole project with sb-mig generate and sb-mig add components'
 
   static examples = [
-    `$ sb-mig generate ?`,
+    `$ sb-mig generate project-name --add @ef-sbc/web-ui-accordion @ef-sbc/web-ui-section`,
   ]
+
+  static strict = false;
 
   static flags = {
     help: flags.help({ char: 'h' }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'n', description: 'name to print' }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
+    add: flags.boolean({ char: 'a', description: 'List of components to add to project' }),
+    copy: flags.boolean({ char: 'c', description: "Copy downloaded files into your folder structure (outside node_modules)." }),
   }
 
-  static args = [{ name: 'file' }]
+  static args = [{ name: 'project-name' }]
 
   async run() {
-    const { args, flags } = this.parse(Hello)
+    const { args, argv, flags } = this.parse(Hello)
     const { boilerplateUrl } = this.storyblokConfig()
 
     console.log(`Starting generating project...`)
     console.log(`Creating start project...`)
     console.log(`Using ${boilerplateUrl} boilerplate...`)
+
+    const components = argv.splice(1, argv.length);
 
     let spinner = ora(`Cloning repo...\n`).start()
     const clonedRepo = await cloneRepo(boilerplateUrl);
@@ -47,47 +48,34 @@ export default class Hello extends Command {
       }
     } = await createSpace(this.api().spaces.createSpace, 'new-space-4')
     spinner.stop()
-    console.log("this is a space: ", space)
 
     removeAndModifyFiles(space)
-    // console.log(`Space ${program.args[0]} has been created.`)
+    console.log(`Space has been created.`)
 
+    if (flags.add && !flags.copy) {
 
+      let spinner = ora(`Installing provided components...\n`).start()
+      const installedComponents = await installProvidedComponents(components);
+      spinner.stop()
 
+      spinner = ora(`Updating components.js file...\n`).start()
+      const data = updateComponentsJs(installedComponents, false);
+      spinner.stop()
+    }
 
-    // const {
-    //   data: { space }
-    // } = await api.createSpace(program.args[0])
+    if (flags.add && flags.copy) {
+      let spinner = ora(`Installing provided components...\n`).start()
+      const installedComponents = await installProvidedComponents(components);
+      spinner.stop()
 
-    // if (args.type === "components" && !flags.copy) {
+      spinner = ora(`Copying components files to local system...\n`).start()
+      const dataAgain = await copyComponents(components);
+      spinner.stop()
 
-    //   let spinner = ora(`Installing provided components...\n`).start()
-    //   const installedComponents = await installProvidedComponents(components);
-    //   spinner.stop()
-
-    //   console.log(installedComponents);
-
-    //   spinner = ora(`Updating components.js file...\n`).start()
-    //   const data = updateComponentsJs(installedComponents, false);
-    //   spinner.stop()
-    // }
-
-    // if (args.type === "components" && flags.copy) {
-
-    //   let spinner = ora(`Installing provided components...\n`).start()
-    //   const installedComponents = await installProvidedComponents(components);
-    //   spinner.stop()
-
-    //   console.log(installedComponents);
-
-    //   spinner = ora(`Copying components files to local system...\n`).start()
-    //   const dataAgain = await copyComponents(components);
-    //   spinner.stop()
-
-    //   spinner = ora(`Updating components.js file...\n`).start()
-    //   const data = updateComponentsJs(installedComponents, true);
-    //   spinner.stop()
-    // }
+      spinner = ora(`Updating components.js file...\n`).start()
+      const data = updateComponentsJs(installedComponents, true);
+      spinner.stop()
+    }
   }
 }
 
