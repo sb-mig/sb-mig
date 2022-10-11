4aa84ee9433539006fc8fa1c0db580dc182a1981
marckraw <marckraw@icloud.com>
2022-10-11 17:32:07 +0200

    fix(console.logs): remove definitelly not needed console.log

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -4,8 +4,6 @@ import { pkg } from "../utils/pkg.js";
 export const debug = async () => {
     console.log("storyblok.config.js: ", config, "\n");
 
-    console.log("using local shit");
-
     if (pkg.type === "module") {
         console.log("U are using new esm stuff. Good for you!");
     } else {
