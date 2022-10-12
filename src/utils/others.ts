d8e425e8473a22a0d56fc42f9fe89e1c05b2efe8
marckraw <marckraw@icloud.com>
2022-10-12 21:06:01 +0200

    fix(datestamp): make datestamp better

--- src/utils/others.ts
+++ src/utils/others.ts
@@ -1,1 +1,2 @@
-export const generateDatestamp = (datestamp: Date) => `${datestamp.getFullYear()}-${datestamp.getMonth()}-${datestamp.getDay()}_${datestamp.getTime()}`
+export const generateDatestamp = (datestamp: Date) =>
+    `${datestamp.getFullYear()}-${datestamp.getMonth()}-${datestamp.getDay()}_${datestamp.getHours()}-${datestamp.getMinutes()}-${datestamp.getSeconds()}`;
