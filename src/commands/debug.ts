5e2cfe8578f6ba9ecb7888d1f9a31fb1698fbe28
marckraw <marckraw@icloud.com>
2022-12-08 13:26:26 +0100

    fix(release): trigger

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -3,6 +3,7 @@ import { pkg } from "../utils/pkg.js";
 
 export const debug = async () => {
     console.log("storyblok.config.js: ", config, "\n");
+    console.log(":)");
 
     if (pkg(`${process.cwd()}/package.json`).type === "module") {
         console.log("U are using new esm stuff. Good for you!");
